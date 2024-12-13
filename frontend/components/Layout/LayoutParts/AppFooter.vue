ba21b02420fe72b7012c599139fc1649e08f5c25
atommasi <atommasi@gmail.com>
2024-12-13 15:11:36 +0000

    update links

--- frontend/components/Layout/LayoutParts/AppFooter.vue
+++ frontend/components/Layout/LayoutParts/AppFooter.vue
@@ -2,11 +2,7 @@
   <v-footer color="primary" padless app>
     <v-row justify="center" align="center" dense no-gutters>
       <v-col class="py-2 text-center white--text" cols="12">
-<<<<<<< HEAD
-        <v-btn color="white" icon href="https://github.com/ATommasi/arcane-recipes" target="_blank">
-=======
         <v-btn color="white" icon href="https://github.com/atommasi/arcane-recipes" target="_blank">
->>>>>>> 2236945ae07b2c7129bf77ced77f307e3d8a5586
           <v-icon>
             {{ $globals.icons.github }}
           </v-icon>
